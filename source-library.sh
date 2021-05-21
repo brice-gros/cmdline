c74d61df30c6207b88180fe0b2ca5e04caba33b5
Brice Gros <brice-gros@users.noreply.github.com>
2021-05-21 15:57:28 +0200

    add is_admin and enable windows symlink

--- source-library.sh
+++ source-library.sh
@@ -81,10 +81,44 @@ current_script_real_dir() {
 }
 
 is_ssh_key_password_protected() {
-    local id_rsa_path="$1"
-    if SSH_ASKPASS=/bin/false ssh-keygen -y -f $id_rsa_path < /dev/null &> /dev/null ; then
+    id_rsa_path="$1"
+    if SSH_ASKPASS=/bin/false ssh-keygen -y -f $id_rsa_path < /dev/null >&/dev/null ; then
         return 1
     else
         return 0
     fi
+}
+
+is_admin() {
+  isadmin=1
+  if is_windows_system; then
+    # https://stackoverflow.com/a/16285248/6265375
+    net session >&/dev/null
+    isadmin=$?
+  else
+    # assume sudo available
+    sudo -l >&/dev/null
+    isadmin=$?
+  fi
+  [ $isadmin -eq 0 ] && echo admin || echo user >&2
+  return $isadmin
+}
+
+
+enable_native_symlinks() {
+  # force cygwin to make NTFS symbolic links
+  # http://stackoverflow.com/a/18659632
+  # https://cygwin.com/cygwin-ug-net/using.html#pathnames-symlinks
+  # - Cygwin creates symbolic links potentially in multiple different ways:
+  #   - The default symlinks are plain files containing a magic cookie followed by the path to which the link points. [...]
+  #   - The shortcut style symlinks [*.lnk] [...] is created if the environment variable CYGWIN [...] is set to contain the string winsymlinks or winsymlinks:lnk. [...]
+  #   - Native Windows symlinks [...] are only created if the user explicitely requests creating them. This is done by setting the environment variable CYGWIN to contain the string winsymlinks:native or winsymlinks:nativestrict. [...]
+  # It allows to use `ln -s` instead of `cmd /c mklink`
+  # Works for users with require SeCreateSymbolicLinkPrivilege right on windows run secpol.msc then go Security Settings/Local Policies/User Rights Assignment/"Create symbolic links" (cf. http://superuser.com/a/125981)
+  # So work for admin by default! or for user with Windows 10 (since Creator Update) in Developer mode
+  if is_cygwin ; then
+    export CYGWIN="winsymlinks:nativestrict $CYGWIN"
+  elif is_msys ; then
+    export MSYS="winsymlinks:nativestrict $MSYS"
+  fi
 }
