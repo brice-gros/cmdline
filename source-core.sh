989877aca7b49d2bdfe2c7a346fd4f1e4e53048a
Brice Gros <brice-gros@users.noreply.github.com>
2021-06-04 09:39:34 +0200

    fix color prompt

--- source-core.sh
+++ source-core.sh
@@ -92,17 +92,21 @@ setcoloredprompt() {
   # git bash default works well with black background:
   # export PS1="\[\033]0;$TITLEPREFIX:$PWD\007\]\n\[\033[32m\]\u@\h \[\033[35m\]$MSYSTEM \[\033[33m\]\w\[\033[36m\]`__git_ps1`\[\033[0m\]\n$ "
   # mine works well with black/grey/white background: (Note that \e == \033)
-  export PS1="\[\e[32m\]\u@\h \[\e[35m\]$MSYSTEM \[\e[31m\]\w\[\e[36m\]`__git_ps1`\[\e[0m\]\n$ "
+  systemname=$(uname)
+  if is_msys ; then
+    systemname=$MSYSTEM
+  fi
+  export PS1="\[\e[32m\]\u@\h \[\e[35m\]$systemname \[\e[31m\]\w\[\e[36m\]`__git_ps1`\[\e[0m\]\n$ "
 }
 
 settitlepath() {
   # from git bash default originally:
   # export PS1="\[\033]0;$TITLEPREFIX:$PWD\007\]\n$PS1"
+  systemname=$(uname)
   if is_msys ; then
-    export PS1="\[\e]0;$MSYSTEM \u@\h \w\a\]\n$PS1"
-  else
-    export PS1="\[\e]0;$(uname) \u@\h \w\a\]\n$PS1"
+    systemname=$MSYSTEM
   fi
+  export PS1="\[\e]0;$systemname \u@\h \w\a\]\n$PS1"
 }
 
 bell() {
