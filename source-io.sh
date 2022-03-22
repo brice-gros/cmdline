3eeae49785e42d6f7ca9775f1091487532f62625
Brice Gros <brice-gros@users.noreply.github.com>
2022-03-22 19:00:16 +0100

    Fix color order in colorlog

--- source-io.sh
+++ source-io.sh
@@ -4,8 +4,8 @@
 colorlog () {
     # sed --unbuffered
     sed -u \
-        -e 's/\(.*SUCCE.*\)/\x1b[32m\1\x1b[39m/i' \
         -e 's/\(.*FAIL.*\)/\x1b[31m\1\x1b[39m/i' \
+        -e 's/\(.*ERROR.*\)/\x1b[31m\1\x1b[39m/i' \
         -e 's/\(.*WARN.*\)/\x1b[33m\1\x1b[39m/i' \
-        -e 's/\(.*ERROR.*\)/\x1b[31m\1\x1b[39m/i'
+        -e 's/\(.*SUCCE.*\)/\x1b[32m\1\x1b[39m/i'
 }
