e62f4cb1772f3d0182675c5fdcbffde5e1022782
Brice Gros <brice-gros@users.noreply.github.com>
2022-04-12 14:28:16 +0200

    add use_win_python

--- source-dev.sh
+++ source-dev.sh
@@ -18,3 +18,26 @@ use_nvm() {
         [ -s "$NVM_DIR/bash_completion" ] && source "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
     fi
 }
+
+
+ruby_unbuffered() {
+  $(which ruby) -e "STDOUT.sync=true" -e "STDIN.sync=true" -e "STDERR.sync=true" -e "load(\$0=ARGV.shift)" $@ #unbuffered output
+}
+
+python_unbuffered() {
+  $(which python) -u $@ #unbuffered output
+}
+
+use_win_python() {
+    if is_msys ; then
+      if [[ $1 == 2* ]]; then
+        export PATH=/c/Python27/Scripts:/c/Python27:$PATH
+      elif [[ $1 == 3* ]]; then
+        export PATH=/c/Python$1/Scripts:/c/Python$1:$PATH
+        if [ ! -L /c/Python$1/python3.exe ]; then
+          ln -s /c/Python$1/python.exe /c/Python$1/python3.exe
+        fi
+      fi
+      #alias python3=py_unbuffered
+    fi
+}
